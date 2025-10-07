4ececa5c78e475cd9962169231b5e1f4b33dbbf2
GregorioP06 <g.pescucci2@studenti.unipi.it>
2025-10-07 11:03:33 +0200

    correzione

--- lez04/grafico_funzione.js
+++ lez04/grafico_funzione.js
@@ -1,18 +1,18 @@
 function f(x) {
-    return x ** 3;
+    return Math.sin(x);
 }
 
 let minX = -10;
 let maxX = 10;
 let minY = -5;
 let maxY = 5;
-let r = 1;
+let r = 0.5;
 
 graph = "";
 
 for (let y = maxY; y >= minY; y -= r) {
     for (let x = minX; x <= maxX; x += r) {
-        if (Math.round(f(x)) == y) {
+        if (Math.abs(f(x) - y) <= r / 2) {
             graph += "*";
         } else if (y == 0 && x == 0) {
             graph += "┼";
