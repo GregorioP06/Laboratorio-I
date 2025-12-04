0241c2319a090b88b133c6e4c26d4acc89dc70bd
GregorioP06 <g.pescucci2@studenti.unipi.it>
2025-12-04 13:05:20 +0100

    es evo

--- esercitazioni/esercizi.js
+++ esercitazioni/esercizi.js
@@ -4,3 +4,45 @@ function ack(p, q) {
     if (q === 0) return ack(p - 1, 1);
     return ack(p - 1, ack(p, q - 1));
 }
+
+// let albero = {
+//     val: 1,
+//     sx: {
+//         val: 2,
+//     },
+//     dx: {
+//         val: 3,
+//         sx: { val: 11 },
+//     },
+// };
+function contaDispari(t) {
+    if (t) {
+        if (t.val % 2 === 1) return 1 + contaDispari(t.sx) + contaDispari(t.dx);
+        return contaDispari(t.sx) + contaDispari(t.dx);
+    }
+    return 0;
+}
+// console.log(contaDispari(albero));
+
+function tagliaRami(t, v) {
+    if (t) {
+        for (const i in t.figli) {
+            if (t.figli[i].val % v === 0) t.figli[i] = null;
+            else tagliaRami(t.figli[i], v);
+        }
+        if (t.figli) t.figli = t.figli.filter((x) => x);
+    }
+}
+
+function valuta(t) {
+    if (t) {
+        if (t.figli) {
+            let peso_figli = 0;
+            for (let i in t.figli) {
+                peso_figli += valuta(t.figli[i]);
+            }
+            return t.val(peso_figli);
+        }
+        return t.val;
+    }
+}
